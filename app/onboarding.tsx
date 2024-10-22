989c04858525fc5ce7f65032f46834225ac39cc5
waqas-on-github <waqasvu892@gmail.com>
2024-10-22 13:21:06 +0500

    ✔️ Add image and onboarding text

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { theme } from "../theme";
 import { useRouter } from "expo-router";
 import { useUserStore } from "../store/userStore";
 import { PlantlyButton } from "../components/PlantlyButton";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "../components/PlantlyImage";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -22,6 +23,14 @@ export default function OnboardingScreen() {
       style={styles.container}
     >
       <StatusBar style="light" />
+      <StatusBar style="light" />
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagline}>
+          Keep your plants healthy and hydrated
+        </Text>
+      </View>
+      <PlantlyImage />
       <PlantlyButton title="let me in !" onPress={handlePress} />
     </LinearGradient>
   );
@@ -33,5 +42,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: "center",
   },
 });
